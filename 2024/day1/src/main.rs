f8355e85bb6ee95eaa879df05f930da210cdd57e
Defyus <defyus.all.now@gmail.com>
2024-12-02 09:51:54 -0500

    (fix:cleanup) day1.part2 complete

--- 2024/day1/src/main.rs
+++ 2024/day1/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, num::ParseIntError};
+use std::{fs, num::ParseIntError, ops::Mul};
 
 fn main() -> Result<(), ParseIntError> {
     let input =
@@ -19,16 +19,34 @@ fn main() -> Result<(), ParseIntError> {
     list_b.sort();
 
     let mut distance = 0;
+    let mut similarity = 0;
 
     for i in 0..list_a.len() {
         let a: i32 = list_a[i];
+
+        let mut similar = 0;
+
+        for item in list_b.iter() {
+            if a == *item {
+                similar += 1;
+                println!("{:?} - {:?}", a, item);
+            }
+        }
+
+        similarity += a.mul(similar);
+
         let b: i32 = list_b[i];
+
         distance += a.abs_diff(b)
     }
 
     assert!(2904518 == distance);
 
     println!("Distance: {}", distance);
+
+    assert!(18650129 == similarity);
+
+    println!("Similarity Score: {}", similarity);
 
     Ok(())
 }
